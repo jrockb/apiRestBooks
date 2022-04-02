72b8f777c52f88e9f81aff0388056d2a88bb4f17
Jonny Ferney Bernal Hernández <40707167+jrockb@users.noreply.github.com>
2022-04-02 16:25:07 -0500

    Creación de api rest con Spring

---
+++ src/main/java/co/com/jcd/apirestbooks/backend/ApiRestBooksApplication.java
@@ -0,0 +1,13 @@
+package co.com.jcd.apirestbooks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/jcd/apirestbooks/backend/ApiRestBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jcd.apirestbooks.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
