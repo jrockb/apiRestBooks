de6cd3b8973778e36075ec9b557d9c33a24a6a55
Jonny Ferney Bernal Hernández <40707167+jrockb@users.noreply.github.com>
2022-04-07 12:03:16 -0500

    Agregadas confguraciones para llave primaria y relación con la tabla categoria

--- src/main/java/co/com/jcd/apirestbooks/backend/model/Libro.java
+++ src/main/java/co/com/jcd/apirestbooks/backend/model/Libro.java
@@ -3,7 +3,14 @@ package co.com.jcd.apirestbooks.backend.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name= "libros")
@@ -14,9 +21,13 @@ public class Libro implements Serializable{
 	 */
 	private static final long serialVersionUID = -5577720400147217205L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String descripcion;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 	private Categoria categoria;
 	
 	
